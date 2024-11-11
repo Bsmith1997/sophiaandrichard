4e2cf7b0de56425b15cf075c9e89b3bcf2ab80c7
Bsmith1997 <babette.smith@mail.mcgill.ca>
2024-11-11 18:22:26 -0500

    working locally, same issue deployed

---
+++ app/app/landing/landing.controller.js
@@ -0,0 +1,127 @@
+(function(angular, window) {
+    'use strict';
+    angular.module('sophiaAndRichard')
+        .controller('LandingCtrl', LandingCtrl);
+
+    function LandingCtrl($scope, GoogleMapInit, $timeout, $uibModal) {
+        var vm = this;
+
+        var fixOurStory = function() {
+            var listItems = $('.how-it-works-li');
+            var maxHeight = undefined;
+            listItems.each(function() {
+                var currentItem = $(this);
+                if(!maxHeight ||currentItem.outerHeight() > maxHeight) {
+                    maxHeight = currentItem.outerHeight();
+                }
+            });
+
+            listItems.each(function() {
+                var currentItem = $(this);
+                if(currentItem.outerHeight() !== maxHeight) {
+                    currentItem.css('min-height', maxHeight + 'px');
+                }
+            });
+        };
+
+        vm.mapsApiFailed = false;
+
+        vm.openEdytaGallery = openEdytaGallery;
+
+        vm.openRenGallery = openRenGallery;
+
+        vm.openRoomBlockModal = openRoomBlockModal;
+
+        $scope.$on('$viewContentLoaded', function() {
+            window.tak.runUripV2();
+            GoogleMapInit.mapsInitialized.then(function() {
+                var directionsDisplay = new google.maps.DirectionsRenderer;
+                var directionsService = new google.maps.DirectionsService;
+
+                // make the map after the digest cycle is complete
+                $timeout(function() {
+                    var mapDiv = document.getElementById('googleMap');
+                    var map = new google.maps.Map(mapDiv, {
+                        center: {lat: 36.115662, lng: -115.170364},
+                        zoom: 12,
+                        scrollwheel: false
+                    });
+                    directionsDisplay.setMap(map);
+                    directionsDisplay.setPanel(document.getElementById('right-panel'));
+
+                    directionsService.route({
+                        origin: '3555 S Las Vegas Blvd, Las Vegas, NV 89109',
+                        destination: '8310 S Rainbow Blvd #100, Las Vegas, NV 89139',
+                        travelMode: 'DRIVING'
+                    }, function(response, status) {
+                        if (status === 'OK') {
+                            directionsDisplay.setDirections(response);
+                        } else {
+                            // Show iframe instead
+                            $scope.$apply(vm.mapsApiFailed = true);
+                        }
+                    });
+                });
+            });
+
+            fixOurStory();
+        });
+
+        $(window).resize(fixOurStory);
+
+        function openRoomBlockModal() {
+            $uibModal.open({
+                animation: true,
+                templateUrl: 'app/landing/room-block-modal.html',
+                controller: 'RoomBlockModalCtrl',
+                controllerAs: 'vm',
+                size: 'lg'
+            });
+        }
+
+        function openEdytaGallery(idx) {
+            var images =  [
+                {id: 0, image: 'images/thumbnail-strip/0003-lrg.jpg'},
+                {id: 1, image: 'images/thumbnail-strip/0020-lrg.jpg'},
+                {id: 2, image: 'images/thumbnail-strip/0039-lrg.jpg'},
+                {id: 3, image: 'images/thumbnail-strip/0051-lrg.jpg'},
+                {id: 4, image: 'images/thumbnail-strip/0116-lrg.jpg'},
+                {id: 5, image: 'images/thumbnail-strip/0147-lrg.jpg'}
+            ];
+            openGalleryModal(idx, images);
+        }
+
+        function openRenGallery(idx) {
+            var images =  [
+                {id: 0, image: 'images/engagement/_REN1339-Edit-lrg.jpg'},
+                {id: 1, image: 'images/engagement/_REN1369-Edit-lrg.jpg'},
+                {id: 2, image: 'images/engagement/_REN1656-lrg.jpg'},
+                {id: 3, image: 'images/engagement/_REN1690-Edit-lrg.jpg'},
+                {id: 4, image: 'images/engagement/_REN1708-Edit-lrg.jpg'},
+                {id: 5, image: 'images/engagement/_REN1722-Edit-lrg.jpg'},
+                {id: 6, image: 'images/engagement/_REN1788-lrg.jpg'},
+                {id: 7, image: 'images/engagement/_REN1800-lrg.jpg'},
+                {id: 8, image: 'images/engagement/_REN1853-Edit-lrg.jpg'}
+            ];
+            openGalleryModal(idx, images);
+        }
+
+        function openGalleryModal(idx, images) {
+            $uibModal.open({
+                animation: true,
+                templateUrl: 'app/landing/gallery-modal.html',
+                controller: 'GalleryModalCtrl',
+                controllerAs: 'vm',
+                size: 'lg',
+                resolve: {
+                    startIdx: function() {
+                        return idx;
+                    },
+                    images: function () {
+                        return images;
+                    }
+                }
+            });
+        }
+    }
+})(angular, window);
---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+const gulp = require('gulp');
+const del = require('del');
+const merge = require('merge-stream');
+const minifyCss = require('gulp-minify-css');
+const uglify = require('gulp-uglify');
+const ngAnnotate = require('gulp-ng-annotate');
+const gulpif = require('gulp-if');
+const revReplace = require('gulp-rev-replace');
+const useref = require('gulp-useref');
+const rev = require('gulp-rev');
+const htmlmin = require('gulp-htmlmin');
+
+// delete everything in the dist folder
+function cleanDist() {
+    return del(['dist/**/*']);
+}
+
+// copy over images, fonts, and other static files
+function copyFiles() {
+    const imgs = gulp.src('app/images/**/*')
+        .pipe(gulp.dest('dist/images'));
+
+    // copy and minify css files
+    const cssFiles = ['app/css/swipebox.css', 'app/css/style-v2.css', 'app/css/style-responsive-v2.css'];
+    const css = gulp.src(cssFiles)
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dist/css'));
+
+    const preMinifiedCss = gulp.src('app/css/animate.min.css')
+        .pipe(gulp.dest('dist/css'));
+
+    const fonts = gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'));
+
+    // copy over minified JS files
+    const minifiedJSArray = [
+        'app/js/minified/SmoothScroll.min.js', 
+        'app/js/minified/classie.min.js',
+        'app/js/minified/jquery.nav.min.js', 
+        'app/js/minified/jquery.swipebox.min.js', 
+        'app/js/minified/expandableGallery.min.js', 
+        'app/js/minified/jquery.counterup.min.js',
+        'app/js/minified/jquery-css-transform.min.js', 
+        'app/js/minified/jquery-animate-css-rotate-scale.min.js',
+        'app/js/minified/jquery.quicksand.min.js', 
+        'app/js/minified/headhesive.min.js',
+        'app/js/minified/scrollReveal.min.js', 
+        'app/js/minified/jquery.countdown.min.js'
+    ];
+    
+    const preMinifiedJS = gulp.src(minifiedJSArray)
+        .pipe(gulp.dest('dist/js/minified'));
+
+    const jsFiles = gulp.src(['app/js/modernizr.js', 'app/js/v2.js', 'app/js/jquery.stellar.js'])
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+
+    const uiBootstrap = gulp.src('app/js/ui-bootstrap/ui-bootstrap-custom-tpls-2.0.1.tak.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/ui-bootstrap'));
+
+    const templates = gulp.src('app/app/**/*.html')
+        .pipe(htmlmin({ removeComments: true, collapseWhitespace: true, conservativeCollapse: true }))
+        .pipe(gulp.dest('dist/app'));
+
+    return merge(imgs, css, preMinifiedCss, preMinifiedJS, uiBootstrap, jsFiles, templates, fonts);
+}
+
+// build the HTML
+function buildHtml() {
+    const condition = function (file) {
+        const filesToRev = {
+            'vendor.css': true,
+            'app.js': true,
+            'vendor.js': true,
+            'app.css': true
+        };
+        return filesToRev[file.basename];
+    };
+
+    return gulp.src('app/index.html')
+        .pipe(useref()) // Concatenate with gulp-useref
+        .pipe(gulpif('app/*.js', ngAnnotate()))
+        .pipe(gulpif('app/*.js', uglify()))
+        .pipe(gulpif('css/*.css', minifyCss())) // Minify vendor CSS sources
+        .pipe(gulpif(condition, rev())) // Rename the concatenated files
+        .pipe(revReplace()) // Substitute in new filenames
+        .pipe(gulp.dest('dist'));
+}
+
+// minify index HTML
+function minifyIndexHtml() {
+    return gulp.src('dist/index.html')
+        .pipe(htmlmin({ removeComments: true }))
+        .pipe(gulp.dest('dist'));
+}
+
+// register tasks
+gulp.task('clean-dist', cleanDist);
+gulp.task('copy-files', copyFiles);
+gulp.task('build-html', buildHtml);
+gulp.task('minify-index-html', minifyIndexHtml);
+
+// define build task using series and parallel
+gulp.task('build', gulp.series(
+    'clean-dist',
+    gulp.parallel('copy-files'),
+    'build-html',
+    'minify-index-html'
+));
